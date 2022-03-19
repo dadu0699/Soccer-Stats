da4b1fe2acfb1b8a39454eafe98ab3ea422d85c9
“Christopher <gudiel.lv16@gmail.com>
2022-03-19 05:21:57 -0600

    [ADD] Funcionalidad de CRUD para competicion

--- Services/administrative_server/src/routes/competencia_routes.js
+++ Services/administrative_server/src/routes/competencia_routes.js
@@ -2,27 +2,157 @@ const {Router} = require("express");
 const router = Router();
 
 const pool = require('../db_connection');
+const jwt = require('jsonwebtoken');
 
-router.post('/competition', (req, res) => {
-    res.send("Hello World - crear competition")
-});
-
-router.put('/competition', (req, res) => {
-    res.send("Hello World - actualizar competition")
-});
-
-router.get('/competition', (req, res) => {
-
-    //si viene championship en query params, solo esa
-    //sino todos
-
-    res.send("Hello World - ver competition")
-});
-
-router.delete('/competition', (req, res) => {
+router.post('/competition', [verificarToken,isAdminOrEmployee], (req, res) => {
     
-    // viene id en params
-    res.send("Hello World - eliminar competition")
+    const {name, type, year, champion_team, country} = req.body;
+
+    let sql = "INSERT INTO Competencia (nombre, anio, tipo, equipoCampeonID, paisID) VALUES(?,?,?,?,?);";
+
+    try {
+        //insertando a DB, sin URL de foto aun
+        pool.query(sql, [name,year,type,champion_team,country], function(err,result){
+            if(err){
+                console.log(err);
+                res.status(200).json({status:500, msj: "Error al agregar Competencia", data: "Error al agregar Competencia"});
+            }else{
+                res.status(200).json({status:200, msj: "Competencia agregada", data: "Competencia agregada"});
+            }
+        });
+    } catch (error) {
+        res.status(200).json({status:500, msj: "Error al agregar Competencia", data: "Error al agregar Competencia"});
+    }
 });
+
+router.put('/competition', [verificarToken,isAdminOrEmployee], (req, res) => {
+    
+    const {id_competition, name, type, year, champion_team, country} = req.body;
+
+    let sql = "UPDATE Competencia SET nombre=?, anio=?, tipo=?, equipoCampeonID=?, paisID=? WHERE competenciaID = ?";
+
+    try {
+        //insertando a DB, sin URL de foto aun
+        pool.query(sql, [name,year,type,champion_team,country,id_competition], function(err,result){
+            if(err){
+                console.log(err);
+                res.status(200).json({status:500, msj: "Error al actualizar Competencia", data: "Error al actualizar Competencia"});
+            }else{
+                res.status(200).json({status:200, msj: "Competencia actualizada", data: "Competencia actualizada"});
+            }
+        });
+    } catch (error) {
+        res.status(200).json({status:500, msj: "Error al actualizar Competencia", data: "Error al actualizar Competencia"});
+    }
+});
+
+router.get('/competition/:championship', [verificarToken,isAdminOrEmployee], (req, res) => {
+
+    const id = req.params.championship;
+
+    var sql = `SELECT Competencia.competenciaID as "id_competition", Competencia.nombre as "name", Competencia.anio as "year", 
+               Competencia.tipo as "type", Competencia.equipoCampeonID as "champion_team ", Competencia.paisID as "id_country ", Pais.nombre2 as "country" 
+               FROM Competencia 
+               JOIN Pais ON Pais.paisID = Competencia.paisID AND Competencia.competenciaID = ${id};`
+
+    try {
+        pool.query(sql,function(err, result, fields){
+            if (err) {
+                res.status(200).json({status:500, msj: "Error al leer Competencia", data: "Error al leer Competencia"});
+            }else{
+                res.status(200).json({status:200, msj: "Competencia leida", data: result});
+            }
+        });
+    } catch (error) {
+        res.status(200).json({status:500, msj: "Error al leer Competencia", data: "Error al leer Competencia"});
+    }
+});
+
+router.get('/competition', [verificarToken,isAdminOrEmployee], (req, res) => {
+
+    var sql = ` SELECT Competencia.competenciaID as "id_competition", Competencia.nombre as "name", Competencia.anio as "year", 
+                Competencia.tipo as "type", Competencia.equipoCampeonID as "champion_team ", Competencia.paisID as "id_country ", Pais.nombre2 as "country" 
+                FROM Competencia 
+                JOIN Pais ON Pais.paisID = Competencia.paisID;`
+
+    try {
+        pool.query(sql,function(err, result, fields){
+            if (err) {
+                res.status(200).json({status:500, msj: "Error al leer Competencias", data: "Error al leer Competencias"});
+            }else{
+                res.status(200).json({status:200, msj: "Competencias leidas", data: result});
+            }
+        });
+    } catch (error) {
+        res.status(200).json({status:500, msj: "Error al leer Competencias", data: "Error al leer Competencias"});
+    }
+});
+
+router.delete('/competition/:id', [verificarToken,isAdminOrEmployee], (req, res) => {
+    
+    const id = req.params.id;
+
+    let sql = 'DELETE FROM Competencia WHERE competenciaID = ?';
+
+    try {
+        //insertando a DB, sin URL de foto aun
+        pool.query(sql, [id], function(err,result){
+            if(err){
+                console.log(err);
+                res.status(200).json({status:500, msj: "Error al eliminar Competencia", data: "Error al eliminar Competencia"});
+            }else{
+                res.status(200).json({status:200, msj: "Competencia eliminada", data: "Competencia eliminada"});
+            }
+        });
+    } catch (error) {
+        res.status(200).json({status:500, msj: "Error al eliminar Competencia", data: "Error al eliminar Competencia"});
+    }
+});
+
+function verificarToken(req, res, next){
+    //valido si viene cabecera de autorizacion
+    if(!req.headers.authorization){
+        return res.status(200).json({status:401, msj: "No autorizado", data: "No autorizado"});
+    }
+
+    //valido si no esta vacio
+    const token = req.headers.authorization.split(' ')[1];
+    if(token==='null'){
+        return res.status(200).json({status:401, msj: "No autorizado", data: "No autorizado"});
+    }
+
+    try {
+        //Verifico token
+        const contenido = jwt.verify(token, process.env.SECRET_JWT_SEED);
+        
+        if(!contenido){
+            return res.status(200).json({status:401, msj: "No autorizado", data: "No autorizado"});
+        }
+
+        next();
+    } catch (error) {
+        console.log(error);
+        return res.status(200).json({status:401, msj: "No autorizado", data: "No autorizado"});
+    }
+}
+
+function isAdminOrEmployee(req, res, next){
+       
+    try {
+        //ya no valida si viene cabecera o si es null, la funcion verificarToken se encarga de eso
+        const token = req.headers.authorization.split(' ')[1];
+        const contenido = jwt.verify(token, process.env.SECRET_JWT_SEED);
+
+        if(contenido.id_rol == 1 || contenido.id_rol == 2){
+            next();
+        }else{
+             return res.status(200).json({status:401, msj: "No autorizado", data: "No autorizado"});
+        }
+
+    } catch (error) {
+        console.log(error);
+        return res.status(200).json({status:401, msj: "No autorizado", data: "No autorizado"});
+    }
+}
 
 module.exports = router;
