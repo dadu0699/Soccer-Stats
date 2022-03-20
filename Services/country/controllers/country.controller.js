900d68450c2a6b98630705be7b85b0599a3df73d
María Reneé Juárez Albizures <debymrj@gmail.com>
2022-03-20 17:22:01 -0600

    [UPDATE] envio de errores

--- Services/country/controllers/country.controller.js
+++ Services/country/controllers/country.controller.js
@@ -3,12 +3,12 @@ const countryModel = require("../models/country.model");
 
 obtenerPaises = (req, res) => {
   countryModel.get(req, (err, results) => {
-    if (err) return response(res, 400, err, 'Error al obtener los paises.');
-    response(res, 200, results, 'Se han obtenido los paises.');
+    if (err) return response(res, 400, 'Error al obtener los paises.', [err]);
+    response(res, 200, 'Se han obtenido los paises.', results);
   });
 };
 
-const response = (res, code, data, msg) => {
+const response = (res, code, msg, data) => {
   res.status(code).send({ status: code, msg, data });
 };
 
--- Services/post/controllers/post.controller.js
+++ Services/post/controllers/post.controller.js
@@ -3,14 +3,14 @@ const postModel = require("../models/post.model");
 
 obtenerNoticias = (req, res) => {
   postModel.get(req.query, (err, results) => {
-    if (err) return response(res, 400, 'Error al obtener noticia(s).', err);
+    if (err) return response(res, 400, 'Error al obtener noticia(s).', [err]);
     response(res, 200, 'Noticia(s) obtenida(s) con éxito.', results);
   });
 };
 
 crearNoticia = (req, res) => {
   postModel.post(req.body, (err, results) => {
-    if (err) return response(res, 400, 'Error al crear noticia.', err);
+    if (err) return response(res, 400, 'Error al crear noticia.', [err]);
     response(res, 200, 'Noticia creada con éxito.', []);
   });
 };
