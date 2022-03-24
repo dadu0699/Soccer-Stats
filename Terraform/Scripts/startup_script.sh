c3d1fac9047685bf472028aba6136ef77bf734ee
Didier Domínguez <dadu0699@hotmail.com>
2022-03-24 16:18:22 -0600

    [UPDATE] Terraform variables

--- Terraform/Scripts/startup_script.sh
+++ Terraform/Scripts/startup_script.sh
@@ -5,6 +5,8 @@ sudo apt install -y docker.io
 sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 
-cat ${google_credentials_file} | docker login -u _json_key --password-stdin https://gcr.io
+sudo nano ${google_credentials_file} creds.json
+sudo cat creds.json | docker login -u _json_key --password-stdin https://gcr.io
+sudo rm -rf creds.json
 
-docker run -d -p 5000:5000 --restart always --name customer-service ${gcr_id}/customer-service:latest
+sudo docker run -d -p 5000:5000 --restart always --name customer-service ${gcr_id}/customer-service:latest
