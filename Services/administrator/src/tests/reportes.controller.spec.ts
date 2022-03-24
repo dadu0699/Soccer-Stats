dffa994e7ffa2e9bbaf66e6d4bdad6d1cb82e423
José Rafael Morente González <josemorenteg98@gmail.com>
2022-03-24 02:50:32 -0600

    [UPDATE] reporte.controller.spec.ts
    Agregado test del reporte 1.

--- Services/administrator/src/tests/reportes.controller.spec.ts
+++ Services/administrator/src/tests/reportes.controller.spec.ts
@@ -22,5 +22,27 @@ describe('APIs Test Administrator', () => {
         expect(res.statusCode).toEqual(200);
     });
 
+});
 
+describe('APIs Test Reportes', () => {
+
+    let headers = {
+        'authorization': `Bearer ${process.env.TOKEN_ADMINISTRADOR}`,
+        'Accept': 'application/json'
+    }
+
+    test('GET ALL Reporte 1 - Obtener reporte 1 sin parametro.', async () => {
+        const expected = {
+            status: 400,
+            msg: "Error al obtener usuarios suscritos al equipo x.",
+            data: []
+        }
+
+        const res = await request(server.app)
+            .get('/api/report/1')
+            .set(headers);
+
+        // COMPARAR RESPONSE
+        expect(res.body).toEqual(expected);
+    });
 });
