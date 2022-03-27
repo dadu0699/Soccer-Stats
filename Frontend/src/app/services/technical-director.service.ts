44a0d13b79a2d69f1835355e6cfff31e84634a88
José Rafael Morente González <josemorenteg98@gmail.com>
2022-03-27 01:18:20 -0600

    [ADD] technical-director.service.ts
    Agregado servicio de director tecnico.

---
+++ Frontend/src/app/services/technical-director.service.ts
@@ -0,0 +1,45 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+import { environment } from 'src/environments/environment';
+import { TechnicalDirector } from '../models/technical-director.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TechnicalDirectorService {
+  private url: string;
+  private httpOptions = {
+    headers: new HttpHeaders({
+      'Authorization': 'bearer ' + localStorage.getItem('token'),
+      'Content-Type': 'application/json'
+    })
+  };
+
+  constructor(private _httpClient: HttpClient) {
+    this.url = `${environment.url}:5005/technical-director`;
+  }
+
+  public async get(): Promise<any> {
+    return await this._httpClient.
+      get(this.url).toPromise();
+  }
+
+  public async create(technical: TechnicalDirector): Promise<any> {
+    return await this._httpClient.
+      post(this.url, technical, this.httpOptions).toPromise();
+  }
+
+  public async update(technical: TechnicalDirector): Promise<any> {
+    return await this._httpClient.
+      put(this.url, technical, this.httpOptions).toPromise();
+  }
+
+  public async delete(technical: TechnicalDirector): Promise<any> {
+    return await this._httpClient.
+      delete(this.url, {
+        headers: this.httpOptions.headers,
+        body: technical
+      }).toPromise();
+  }
+}
