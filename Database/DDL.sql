4f08a1bfabdd82245865f81e0133c55a79ff60a3
José Rafael Morente González <josemorenteg98@gmail.com>
2022-03-26 18:00:11 -0600

    [UPDATE] DDL.sql
    Agregado estadios con menor capacidad a X, jugadores o directores de equipos, partidos donde hubo al menos X goles.

--- Database/DDL.sql
+++ Database/DDL.sql
@@ -329,3 +329,61 @@ SELECT Estadio.estadioID AS id_stadium, Estadio.nombre AS stadium,
 Estadio.foto AS photo, Estadio.paisID AS id_country, Pais.nombre AS country FROM Estadio
 INNER JOIN Pais ON Pais.paisID = Estadio.paisID
 WHERE Estadio.paisID = 1;
+
+-- ESTADIOS CON CAPACIDAD MENOR O IGUAL A X
+SELECT Estadio.estadioID AS id_stadium, Estadio.nombre AS stadium, Estadio.capacidad AS capacity,
+Estadio.foto AS photo, Estadio.paisID AS id_country, Pais.nombre AS country FROM Estadio
+INNER JOIN Pais ON Pais.paisID = Estadio.paisID
+WHERE Estadio.capacidad <= 25000;
+
+-- HISTORICOS DE PARTIDOS DE X PARTIDO
+SELECT 
+Partido.partidoID AS id, Partido.fechaHora AS game_date, Partido.asistencia AS attendees,
+Partido.resultadoLocal AS result_local, Partido.resultadoVisitante AS result_visiting, 
+Partido.estado AS status, Partido.estadioID AS id_stadium, Estadio.nombre AS stadium,
+Partido.equipoLocalID AS id_team_local, E1.nombre AS team_local, E1.fotoLogo AS photo_local,
+Partido.equipoVisitaID AS id_team_visiting, E2.nombre AS team_visiting, E2.fotoLogo AS photo_visiting,
+Partido.competenciaID AS id_competition, Competencia.nombre AS competition
+FROM Partido
+INNER JOIN Equipo AS E1 ON E1.equipoID = Partido.equipoVisitaID
+INNER JOIN Equipo AS E2 ON E2.equipoID = Partido.equipoLocalID
+INNER JOIN Estadio ON Estadio.estadioID = Partido.estadioID
+INNER JOIN Competencia ON Competencia.competenciaID = Partido.competenciaID
+WHERE Partido.equipoVisitaID = 1 OR Partido.equipoLocalID = 1; -- Enviar el mismo id de equipo en local y visitante
+
+-- EQUIPOS DONDE HA ESTADO O DIRIGIDO X JUGADOR O DIRECTOR
+SELECT DirectorTecnico.directorTecnicoID AS id, DirectorTecnico.nombre AS name, 
+DirectorTecnico.foto AS photo, DirectorTecnico.apellido AS lastname,
+Pais.nombre AS nacionality, NULL AS position, 
+ContratoDT.fechaInicio AS start_date, ContratoDT.fechaFin AS end_date
+FROM ContratoDT
+INNER JOIN Equipo ON Equipo.equipoID = ContratoDT.equipoDestinoID
+INNER JOIN DirectorTecnico ON DirectorTecnico.directorTecnicoID = ContratoDT.directorTecnicoID
+INNER JOIN Pais ON Pais.paisID = DirectorTecnico.paisID
+WHERE Equipo.equipoID = 1;
+
+SELECT Jugador.jugadorID AS id, Jugador.nombre AS name, 
+Jugador.foto AS photo, Jugador.apellido AS lastname,
+Pais.nombre AS nacionality, PosicionJugador.nombre AS position,
+ContratoJugador.fechaInicio AS start_date, ContratoJugador.fechaFin AS end_date
+FROM ContratoJugador
+INNER JOIN Equipo ON Equipo.equipoID = ContratoJugador.equipoDestinoID
+INNER JOIN Jugador ON Jugador.jugadorID = ContratoJugador.jugadorID
+INNER JOIN Pais ON Pais.paisID = Jugador.paisID
+INNER JOIN PosicionJugador ON PosicionJugador.posicionJugadorID = Jugador.posicionJugadorID
+WHERE Equipo.equipoID = 1;
+
+-- PARTIDOS DONDE HUBO AL MENOS X GOLES
+SELECT 
+Partido.partidoID AS id, Partido.fechaHora AS game_date, Partido.asistencia AS attendees,
+Partido.resultadoLocal AS result_local, Partido.resultadoVisitante AS result_visiting, 
+Partido.estado AS status, Partido.estadioID AS id_stadium, Estadio.nombre AS stadium,
+Partido.equipoLocalID AS id_team_local, E1.nombre AS team_local, E1.fotoLogo AS photo_local,
+Partido.equipoVisitaID AS id_team_visiting, E2.nombre AS team_visiting, E2.fotoLogo AS photo_visiting,
+Partido.competenciaID AS id_competition, Competencia.nombre AS competition
+FROM Partido
+INNER JOIN Equipo AS E1 ON E1.equipoID = Partido.equipoVisitaID
+INNER JOIN Equipo AS E2 ON E2.equipoID = Partido.equipoLocalID
+INNER JOIN Estadio ON Estadio.estadioID = Partido.estadioID
+INNER JOIN Competencia ON Competencia.competenciaID = Partido.competenciaID
+WHERE (Partido.resultadoLocal + Partido.resultadoVisitante) <= 3;
