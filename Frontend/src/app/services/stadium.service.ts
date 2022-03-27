e053d28f1c6b3ca70e60c08d8b1738c0a7c34e7d
José Rafael Morente González <josemorenteg98@gmail.com>
2022-03-27 00:10:06 -0600

    [UPDATE] stadium.service.ts, stadium-view.component.ts
    Agregado CRUD y peticiones a estadio.

--- Frontend/src/app/services/stadium.service.ts
+++ Frontend/src/app/services/stadium.service.ts
@@ -1,19 +1,45 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 import { environment } from 'src/environments/environment';
+import { Stadium } from '../models/stadium.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class StadiumService {
   private url: string;
+  private httpOptions = {
+    headers: new HttpHeaders({
+      'Authorization': 'bearer ' + localStorage.getItem('token'),
+      'Content-Type': 'application/json'
+    })
+  };
 
   constructor(private _httpClient: HttpClient) {
     this.url = `${environment.url}:5004/stadium`;
   }
 
   public async get(): Promise<any> {
-    return await this._httpClient.get(this.url).toPromise();
+    return await this._httpClient
+      .get(this.url).toPromise();
+  }
+
+  public async create(stadium: Stadium): Promise<any> {
+    return await this._httpClient.
+      post(this.url, stadium, this.httpOptions).toPromise();
+  }
+
+  public async update(stadium: Stadium): Promise<any> {
+    return await this._httpClient.
+      put(this.url, stadium, this.httpOptions).toPromise();
+  }
+
+  public async delete(stadium: Stadium): Promise<any> {
+    return await this._httpClient.
+      delete(this.url, {
+        headers: this.httpOptions.headers,
+        body: stadium
+      }).toPromise();
   }
 }
