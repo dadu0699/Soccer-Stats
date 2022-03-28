570c3998b37f906b417d76ce06050b444fe56c96
Didier Domínguez <dadu0699@hotmail.com>
2022-03-27 19:43:35 -0600

    [UPDATE] Equipos en el director tecnico

--- Services/technical_director/src/controllers/director-tecnico.controller.ts
+++ Services/technical_director/src/controllers/director-tecnico.controller.ts
@@ -1,5 +1,7 @@
 import { Request, Response } from 'express';
 import CryptoJS = require("crypto-js");
+import db from "../db/connection";
+import { QueryTypes } from 'sequelize';
 import UploadFile from '../utils/aws-s3.util';
 import DirectorTecnico from '../models/director-tecnico.model';
 import BitacoraController from './bitacora.controller';
@@ -20,10 +22,11 @@ export default class DirectorTecnicoController {
     getAll = async (req: Request, res: Response) => {
         const { id } = req.query;
 
+        let query = 'SELECT * FROM vistaDirectores';
         if (id == null) {
-            const data = await DirectorTecnico.findAll({
-                include: ['pais']
-            });
+            const data = await db.query(query, {
+                type: QueryTypes.SELECT
+            })
 
             let directores = data.map((director: any) => {
                 return {
@@ -33,8 +36,10 @@ export default class DirectorTecnicoController {
                     birth_date: director.fechaNacimiento,
                     status: director.estado,
                     id_country: director.paisID,
-                    country: director.pais.nombre,
+                    country: director.pais,
                     photo: director.foto,
+                    id_team: director.equipoID,
+                    team: director.equipo
                 }
             });
 
@@ -44,23 +49,35 @@ export default class DirectorTecnicoController {
                 data: directores
             });
         } else {
-            const data: any = await DirectorTecnico.findByPk(Number(id), {
-                include: ['pais']
-            });
-            if (data) {
+            query += ' WHERE directorTecnicoID = :id';
+
+            const data = await db.query(query, {
+                replacements: {
+                    id: id
+                },
+                type: QueryTypes.SELECT
+            })
+
+            if (data.length > 0) {
+                let resultado = data.map((director: any) => {
+                    return {
+                        id: director.directorTecnicoID,
+                        name: director.nombre,
+                        lastname: director.apellido,
+                        birth_date: director.fechaNacimiento,
+                        status: director.estado,
+                        id_country: director.paisID,
+                        country: director.pais,
+                        photo: director.foto,
+                        id_team: director.equipoID,
+                        team: director.equipo
+                    }
+                })
+
                 return res.json({
                     status: 200,
                     msg: "Director(es) técnico(s) obtenido(s) con éxito.",
-                    data: [{
-                        id: data.directorTecnicoID,
-                        name: data.nombre,
-                        lastname: data.apellido,
-                        birth_date: data.fechaNacimiento,
-                        status: data.estado,
-                        id_country: data.paisID,
-                        country: data.pais.nombre,
-                        photo: data.foto,
-                    }]
+                    data: resultado
                 });
             } else {
                 return res.status(400).json({
