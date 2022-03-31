6245cbd509ed8647287fdb2748a65d42095cd228
Didier Domínguez <dadu0699@hotmail.com>
2022-03-31 17:55:17 -0600

    [FIX] Obtener todos los favoritos del usuario

--- Services/customer/controllers/favorite.controller.js
+++ Services/customer/controllers/favorite.controller.js
@@ -1,5 +1,17 @@
 const favoriteModel = require('../models/favorite.model');
 
+const verFavoritos = (req, res) => {
+  if (req.query['id_client'] != req.user['id_user'])
+    return res.status(401).send({ status: 401, msg: 'Unauthorized', data: [] });
+
+  favoriteModel.verFavoritos(req.query, (err, results) => {
+    if (err)
+      return response(res, 400, 'Error al obtener los equipos favoritos.', err);
+
+    return response(res, 200, 'Favoritos obtenidos con éxito.', results);
+  });
+};
+
 const agregarFavorito = (req, res) => {
   if (req.body['id_client'] != req.user['id_user'])
     return res.status(401).send({ status: 401, msg: 'Unauthorized', data: [] });
@@ -21,4 +33,4 @@ const response = (res, status, msj, data) => {
   res.status(status).send({ status, msj, data });
 };
 
-module.exports = { agregarFavorito };
+module.exports = { verFavoritos, agregarFavorito };
