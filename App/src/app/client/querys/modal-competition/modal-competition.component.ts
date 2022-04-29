02535b5341f8497c38aed74e306ca666ee42e975
José Rafael Morente González <josemorenteg98@gmail.com>
2022-04-29 09:34:08 -0600

    [UPDATE] Agregado filtro de competiciones.

--- App/src/app/client/querys/modal-competition/modal-competition.component.ts
+++ App/src/app/client/querys/modal-competition/modal-competition.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ModalController } from '@ionic/angular';
+import { CustomerService } from 'src/app/services/customer.service';
+import { NotificacionService } from 'src/app/services/notification.service';
+import { TeamService } from 'src/app/services/team.service';
 
 @Component({
   selector: 'app-modal-competition',
@@ -7,16 +10,63 @@ import { ModalController } from '@ionic/angular';
   styleUrls: ['./modal-competition.component.scss'],
 })
 export class ModalCompetitionComponent implements OnInit {
+  public filter = [];
+  public teams = [];
+  public typesCompetitions = [
+    { id: 1, description: 'Liga' },
+    { id: 2, description: 'Eliminatoria' },
+    { id: 3, description: 'Copa' },
+    { id: 4, description: 'SuperCopa' },
+    { id: 5, description: 'Cuadrangular' },
+    { id: 6, description: 'Triangular' },
+  ];
+  public data = {
+    id_team: 0,
+    array: []
+  }
 
   constructor(
     private modalController: ModalController,
+    private teamService: TeamService,
+    private customerService: CustomerService,
+    private notificacionService: NotificacionService
   ) { }
 
-  ngOnInit() { }
+  ngOnInit() {
+    this.getAllTeams();
+  }
 
   //CERRAR MODAL
   closeModal() {
     this.modalController.dismiss();
   }
+
+  /**
+   * Obtener todos los equipos
+   */
+  getAllTeams() {
+    this.teamService.get()
+      .then((res) => {
+        console.log(res)
+        this.teams = [];
+        this.teams = res.data;
+      });
+  }
+
+  /**
+   * Filtrar juegos
+   */
+  search() {
+    this.customerService.report14(this.data.id_team, this.data.array)
+      .then((res) => {
+        console.log(res)
+        this.filter = [];
+        this.filter = res.data;
+      })
+      .catch((err) => {
+        this.notificacionService.presentToast('An error has ocurred, please try again.');
+      });
+  }
+
 
 }
