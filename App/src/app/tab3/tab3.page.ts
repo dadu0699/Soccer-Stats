5d092aec2fd559442d42b98110d5f7d28282aea4
José Rafael Morente González <josemorenteg98@gmail.com>
2022-05-01 00:52:55 -0600

    [FIX] Arreglado inicio de sesión.

--- App/src/app/tab3/tab3.page.ts
+++ App/src/app/tab3/tab3.page.ts
@@ -51,31 +51,46 @@ export class Tab3Page {
   }
 
   async login() {
-    this.disabledBtn = true;
-    this.authService.signIn(this.data.email, this.data.password).then((res) => {
-      console.log(res)
-      this.disabledBtn = false;
-      if (res.data.id_status == 1) {
-        localStorage.setItem('token', String(res.data.token));
-        localStorage.setItem('has_membership', res.data.has_membership);
-        localStorage.setItem('id_rol', res.data.id_rol);
-        localStorage.setItem('id_status', res.data.id_status);
-        localStorage.setItem('id_user', res.data.id_user);
-        if (this.platform.is('android')) {
-          this.notificacionPushService.configuracionInicial();
+    if (this.validarCampos()) {
+      this.disabledBtn = true;
+      this.authService.signIn(this.data.email, this.data.password).then((res) => {
+        console.log(res)
+        this.disabledBtn = false;
+        if (res.data.id_status == 1) {
+          localStorage.setItem('token', String(res.data.token));
+          localStorage.setItem('has_membership', res.data.has_membership);
+          localStorage.setItem('id_rol', res.data.id_rol);
+          localStorage.setItem('id_status', res.data.id_status);
+          localStorage.setItem('id_user', res.data.id_user);
+          if (this.platform.is('android')) {
+            this.notificacionPushService.configuracionInicial();
+          }
+          if (res.data.id_rol == 2) {
+            this.router.navigate(['/employee']);
+          } else if (res.data.id_rol == 3) {
+            this.router.navigate(['/client']);
+          }
+        } else if (res.data.id_status == 2) {
+          this.notificacionService.presentToast('Your account is pending verification.');
+        } else {
+          this.notificacionService.presentToast('Your account is deactivated.');
         }
-        if (res.data.id_rol == 2) {
-          this.router.navigate(['/employee']);
-        } else if (res.data.id_rol == 3) {
-          this.router.navigate(['/client']);
-        }
-      } else if (res.data.id_status == 2) {
-        this.notificacionService.presentToast('Your account is pending verification.');
-      } else {
-        this.notificacionService.presentToast('Your account is deactivated.');
-      }
-    }).catch(() => {
-      this.disabledBtn = false;
-    });
+      }).catch(() => {
+        this.disabledBtn = false;
+        this.notificacionService.presentToast('Error registering user, please try again.');
+      });
+    }
+  }
+
+  validarCampos(): boolean {
+    if (this.data.email === '' || this.data.email === undefined) {
+      this.notificacionService.presentToast('Email is required.');
+      return false;
+    }
+    if (this.data.password === '' || this.data.password === undefined) {
+      this.notificacionService.presentToast('Password is required.');
+      return false;
+    }
+    return true;
   }
 }
