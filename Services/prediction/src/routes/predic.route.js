6a7e3e1da8febbea87f955d733d8639c570e5c5b
“Christopher <gudiel.lv16@gmail.com>
2022-05-02 04:04:03 -0600

    [ADD] algoritmo de prediccion

--- Services/prediction/src/routes/predic.route.js
+++ Services/prediction/src/routes/predic.route.js
@@ -8,10 +8,69 @@ const {totalPartidos, totalGolesLocal,totalGolesVisitante,
 router.post('/', (req, res) => {
 
     const {id_teamLocal, id_teamVisitor} = req.body;
-    var promedioGolesLocal = 0;
-    let promedioGolesVisitante = 0; 
 
+    let promedioGolesLocal = 0;
+    let promedioGolesVisitante = 0;
+
+    let golesEncajadosEquipoLocal = 0;
+    let golesAnotadosEquipoLocal = 0;
+    let cantidadPartidosComoLocal = 0;
+
+    let golesEncajadosEquipoVisitante = 0;
+    let golesAnotadosEquipoVisitante = 0;
+    let cantidadPartidosComoVisitante = 0;
+
+    let fuerzaAtaqueLocal = 0;
+    let fuerzaDefensaLocal =0;
+
+    let fuerzaAtaqueVisitante = 0;
+    let fuerzaDefensaVisitante =0;
+
+    let golesEsperadosLocal = 0;
+    let golesEsperadosVisitante = 0;
+
+    try {
+        totalPartidos((totalP)=>{
+            totalGolesLocal((totalL) => {
+                totalGolesVisitante((totalV)=>{
+                    promedioGolesLocal = totalL / totalP;
+                    promedioGolesVisitante = totalV / totalP;
+                    //console.log(totalP, totalL, totalV, promedioGolesLocal, promedioGolesVisitante);
+                    equipoLOCAL_goles_encajados_anotados(id_teamLocal, (localGolesE, localGolesA) => {
+                        golesEncajadosEquipoLocal = localGolesE;
+                        golesAnotadosEquipoLocal = localGolesA;
+                        //console.log("team local ",localGolesE, localGolesA);
+                        equipoVISITANTE_goles_encajados_anotados(id_teamVisitor, (visitanteGolesE, visitanteGolesA) => {
+                            golesEncajadosEquipoVisitante = visitanteGolesE;
+                            golesAnotadosEquipoVisitante = visitanteGolesA;
+                            //console.log("team visitor ",visitanteGolesE, visitanteGolesA);
+                            partidosJugadosComoLocal(id_teamLocal, (totalPartidosL) => {
+                                cantidadPartidosComoLocal = totalPartidosL;
+                                //console.log("total partidos local ",totalPartidosL);
+                                partidosJugadosComoVisitante(id_teamVisitor, (totalPartidosV) => {
+                                    //console.log("total partidos visitante ",totalPartidosV);
+                                    cantidadPartidosComoVisitante = totalPartidosV;
     
+                                    fuerzaAtaqueLocal = (golesAnotadosEquipoLocal / cantidadPartidosComoLocal) / promedioGolesLocal;
+                                    fuerzaDefensaLocal = (golesEncajadosEquipoLocal / cantidadPartidosComoLocal) / promedioGolesVisitante;
+    
+                                    fuerzaAtaqueVisitante = (golesAnotadosEquipoVisitante / cantidadPartidosComoVisitante) / promedioGolesVisitante;
+                                    fuerzaDefensaVisitante = (golesEncajadosEquipoVisitante / cantidadPartidosComoVisitante) / promedioGolesLocal;
+    
+                                    golesEsperadosLocal = fuerzaAtaqueLocal * fuerzaDefensaVisitante * promedioGolesLocal;
+                                    golesEsperadosVisitante = fuerzaAtaqueVisitante * fuerzaDefensaLocal * promedioGolesVisitante;
+    
+                                    res.status(200).json({status:200, msg: "Prediccion obtenida con éxito.", data: [{golesLocal: Math.round(golesEsperadosLocal), golesVisitor: Math.round(golesEsperadosVisitante)}]});
+                                });
+                            });                        
+                        });                    
+                    });
+                });
+            });
+        });
+    } catch (error) {
+        res.status(400).json({status:400, msg: "Error al obtener prediccion.", data: [error]});
+    }
 
 });
 
