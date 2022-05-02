1f1b80ca2c0f8689a9fe6f610602dc391c699562
“Christopher <gudiel.lv16@gmail.com>
2022-05-01 22:36:47 -0600

    [ADD] Obtener total de goles visitante

--- Services/prediction/src/models/predic.model.js
+++ Services/prediction/src/models/predic.model.js
@@ -34,4 +34,21 @@ function totalGolesLocal(done) {
     }
 }
 
-module.exports = {totalPartidos, totalGolesLocal};
+function totalGolesVisitante(done) {
+    const sql = "select sum(resultadoVisitante) as total from partido;";
+
+    try {
+        pool.query(sql,function(err, result, fields){
+            if (err) {
+                done(0);
+            }else{
+                const total = result[0].total;
+                done(total);
+            }
+        });
+    } catch (error) {
+        done(0);
+    }
+}
+
+module.exports = {totalPartidos, totalGolesLocal, totalGolesVisitante};
